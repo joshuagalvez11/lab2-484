33a7d9452434c99b3c710a6a810d906fb0ce8360
Joshua Galvez <joshua.galvez.559@my.csun.edu>
2023-04-08 15:42:30 -0700

    date checking functionality

--- script.js
+++ script.js
@@ -29,12 +29,75 @@ form.addEventListener("submit", submitForm);
 
 
 function submitForm(event) {
+    console.log("\nSUBMIT CHECK")
     event.preventDefault();
 
     console.log(names.value)
+    console.log(isCapitalized(names.value)); 
+    
     console.log(date.value)
+    console.log(isDate(date.value))
+
     console.log(calcText.value)
 }
+
+function isCapitalized(str) {
+    const words = str.split(' ');
+  
+    for (let i = 0; i < words.length; i++) {
+      const word = words[i];
+      if (word.length > 0 && word[0] !== word[0].toUpperCase()) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+  
+
+  function isDate(str) {
+    const regexPatterns = [
+        /^(1[0-2]|0[1-9])\/(3[01]|[12][0-9]|0[1-9])\/[0-9]{4}$/,
+        /^(1[0-2]|0[1-9])\\(3[01]|[12][0-9]|0[1-9])\\[0-9]{4}$/,
+        /^(1[0-2]|0[1-9])-(3[01]|[12][0-9]|0[1-9])-[0-9]{4}$/,
+        /^(Jan(uary)?|Feb(ruary)?|Mar(ch)?|Apr(il)?|May|Jun(e)?|Jul(y)?|Aug(ust)?|Sep(tember)?|Oct(ober)?|Nov(ember)?|Dec(ember)?)\s+\d{1,2},\s+\d{4}/
+
+    ];
+  
+    for (let i = 0; i < regexPatterns.length; i++) {
+      if (regexPatterns[i].test(str)) {
+        return true;
+      }
+    }
+  
+    return false;
+  }
+
+// function isDate(str) {
+//     // Define regex patterns for the various date formats
+//     const regexPatterns = [
+//       /^(\d{2})-(\d{2})-(\d{4})$/,
+//       /^(\d{2})\/(\d{2})\/(\d{4})$/,
+//       /^(\d{2})\\(\d{2})\\(\d{4})$/,
+//       /^([A-Za-z]+) (\d{1,2}), (\d{4})$/
+//     ];
+  
+//     // Loop through the regex patterns and check if the input string matches any of them
+//     for (let i = 0; i < regexPatterns.length; i++) {
+//       const matches = str.match(regexPatterns[i]);
+//       if (matches) {
+//         // If a match is found, create a new Date object and return it
+//         const month = matches[1].startsWith('0') ? matches[1].substring(1) : matches[1];
+//         const day = matches[2].startsWith('0') ? matches[2].substring(1) : matches[2];
+//         const year = matches[3];
+//         return true;
+//       }
+//     }
+  
+//     // If no matches are found, return null
+//     return false;
+//   }
+
 
 
 
